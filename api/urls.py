9cf460c7ffbca982a0150072cf045a3d0c55aadb
Mahmoud Darawsheh <darxx03eh@mail.ru>
2025-08-27 15:44:14 +0300

    login, signup and logout created

---
+++ api/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+from api.views.user_views import SignUpAPIView, LogOutAPIView, UserAPIView, UserRetrieveAPIView
+
+urlpatterns = [
+    path('users/me', UserRetrieveAPIView.as_view(), name='retrieve-user'),
+    path('users', UserAPIView.as_view(), name='users'),
+    path('users/signup', SignUpAPIView.as_view(), name='signup'),
+    path('users/login', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('users/logout', LogOutAPIView.as_view(), name='logout'),
+    path('users/refresh', TokenRefreshView.as_view(), name='token_refresh'),
+]
